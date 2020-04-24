c14e733a4e237646d9646f013f6d1fc8f637c501
A I <9pagemake7@gmail.com>
2020-04-24 21:57:36 +0300

    First commit

---
+++ biblyoteka/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from biblyoteka.users.models import User
+from biblyoteka.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
